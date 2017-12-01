5ed2277cee406dcfdd3fa28d9368eed0294ee22c
carmelo12341 <ertsheiker@gmail.com>
2017-11-30 22:16:59 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="root">
+        <h1>All threads</h1>
+        {{ get_threads() }}
+    </div>
+</template>
+
+<script>
+    import nanoajax from "nanoajax"
+    import Thread from "./Thread.vue"
+
+    export default {
+        methods: {
+            get_threads() {
+                nanoajax.ajax({url: "http://127.0.0.1:1337/api/get_threads"}, function (code, response) {
+                    console.log(JSON.parse(response));
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/Thread.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="thread">
+        <h3>{{ this.props.title }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "thread"
+    }
+</script>
+
+<style scoped>
+
+</style>
