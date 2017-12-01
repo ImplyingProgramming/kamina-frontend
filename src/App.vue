5cdc8dfb8824cba0dfa92510a31f16ceadd487ba
carmelo12341 <ertsheiker@gmail.com>
2017-11-30 23:40:51 -0500

    Basic show threads functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-    <div id="root">
+    <div>
         <h1>All threads</h1>
-        {{ get_threads() }}
+        <div v-for="thread_data in threads">
+            <thread v-bind:data="thread_data"></thread>
+        </div>
     </div>
 </template>
 
@@ -10,16 +12,26 @@
     import Thread from "./Thread.vue"
 
     export default {
+        components: {
+            thread: Thread
+        },
+        data() {
+            return {
+                threads: []
+            }
+        },
+        mounted() {
+            this.get_threads();
+        },
         methods: {
             get_threads() {
-                nanoajax.ajax({url: "http://127.0.0.1:1337/api/get_threads"}, function (code, response) {
-                    console.log(JSON.parse(response));
+                nanoajax.ajax({url: "http://127.0.0.1:1337/api/get_threads"}, (code, response) => {
+                    let res = JSON.parse(response);
+                    for (let elem of res) {
+                        this.$data.threads.push(elem);
+                    }
                 });
             }
         }
     }
 </script>
-
-<style>
-
-</style>
--- src/Thread.vue
+++ src/Thread.vue
@@ -1,15 +1,26 @@
 <template>
-    <div id="thread">
-        <h3>{{ this.props.title }}</h3>
+    <div class="thread">
+        <h3>{{ data.title }}</h3>
+        <p>{{ data.body }}</p>
+        <p>id: {{ data.id }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "thread"
+        props: ["data"]
     }
 </script>
 
 <style scoped>
+    .thread h3 {
+        margin: 0;
+    }
 
+    .thread {
+        font-family: Arial, Helvetica, "sans-serif";
+        border: 1px solid black;
+        margin-bottom: 15px;
+        padding: 5px;
+    }
 </style>
