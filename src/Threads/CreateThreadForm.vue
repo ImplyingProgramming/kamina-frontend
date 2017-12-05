1d5817d6a974e1a122a09dea53558676a592286f
carmelo12341 <ertsheiker@gmail.com>
2017-12-04 21:48:12 -0500

    Initial image upload functionality Still needs some issues to be fixed, mostly promises related

--- src/Threads/CreateThreadForm.vue
+++ src/Threads/CreateThreadForm.vue
@@ -7,7 +7,7 @@
         <label for="cthread-body">Body:</label>
         <br><textarea v-model="body" id="cthread-body" cols="30" rows="10"></textarea><br>
         <label for="cthread-file">File:</label>
-        <input type="file" id="cthread-file"><br>
+        <input accept="image/*" type="file" id="cthread-file" @change="bind_file"><br>
         <button v-on:click="make_thread">Create</button>
     </div>
 </template>
@@ -19,26 +19,38 @@
             return {
                 title: "",
                 body: "",
-                media: undefined
+                image: new FormData()
             }
         },
         methods: {
             make_thread() {
                 let thread_title = encodeURIComponent(this.title);
                 let thread_body = encodeURIComponent(this.body);
-                // TODO: Added image upload functionality, somehow
-                // let thread_media = this.media;
+                // TODO: Add image upload functionality, somehow
+                let thread_image = this.image;
+                this.$http.post(
+                    "http://127.0.0.1:1337/api/upload_image",
+                    thread_image
+                ).then(response => {
+                    console.log(response.bodyText);
+                }, error => {
+                    console.error("There was an error uploading the image");
+                });
                 this.$http.post(
                     "http://127.0.0.1:1337/api/make_thread", {
                         title: thread_title,
                         body: thread_body
-                    })
-                    .then(response => {
-                        // TODO: Redirect user to their thread (Needs vue-router)
-                        window.location.reload();
-                    }, error => {
-                        console.error("There was an error creating the thread");
-                    });
+                    }
+                ).then(response => {
+                    // TODO: Redirect user to their thread (Needs vue-router)
+                    // window.location.reload();
+                }, error => {
+                    console.error("There was an error creating the thread");
+                });
+            },
+            bind_file(e) {
+                e.preventDefault();
+                this.image.append("file", e.target.files[0]);
             }
         }
     }
