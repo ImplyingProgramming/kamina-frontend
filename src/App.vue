ec533c2c6531f0c5b5c5189e1d37945e881f963c
carmelo12341 <ertsheiker@gmail.com>
2017-12-08 09:56:00 -0500

    Some changes to App.vue - Modified so that we can show/hide the create thread form - Added some members to the component's data for dynamic board name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="kamina" class="container">
-        <h1 id="board-title">/>p/ - implying we can programming</h1>
-        <cthread-form></cthread-form>
+        <h1 id="board-title">{{ boardLetters }} - {{ boardTitle }}</h1>
+        <button v-on:click="toggle_thread_form">Make a new thread</button>
+        <br><br>
+        <cthread-form v-show="mkthread_btn_pressed"></cthread-form>
         <thread-list></thread-list>
     </div>
 </template>
@@ -14,6 +16,18 @@
         components: {
             "thread-list": ThreadList,
             "cthread-form": CreateThreadForm
+        },
+        data() {
+            return {
+                mkthread_btn_pressed: false,
+                boardLetters: "/>p/",
+                boardTitle: "Implying we can programming"
+            }
+        },
+        methods: {
+            toggle_thread_form() {
+                this.mkthread_btn_pressed = !this.mkthread_btn_pressed;
+            }
         }
     }
 </script>
