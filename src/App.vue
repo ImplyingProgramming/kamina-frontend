ce261d0d528c912b01b2c7943b8c5c1a7660b479
carmelo12341 <ertsheiker@gmail.com>
2017-12-19 12:09:43 -0500

    Changed some names

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-    import ThreadList from "./Threads/ThreadList.vue"
-    import CreateThreadForm from "./Threads/CreateThreadForm.vue"
+    import ThreadList from "./Threads/List.vue"
+    import CreateThreadForm from "./Threads/CreateForm.vue"
 
     export default {
         components: {
